66d6a6350667b4c956f7b92c7062a38138f1515c
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-16 00:13:28 -0200

    feat: Setup airbnb style guide eslint

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initial: 'Hello',
+    };
+  }
   render() {
+    const { initial } = this.state;
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>{initial}</h1>
       </div>
     );
   }
