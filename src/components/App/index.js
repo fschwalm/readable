597fbaf1eb7683b7b564a7287d013a4a842429c4
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-02-21 00:20:32 -0300

    feat: Draft UI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { getPosts, getCategories } from '../../api/ReadableAPI';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initial: 'Hello',
+      posts: [],
     };
   }
+
+  async componentDidMount() {
+    try {
+      const posts = await getPosts();
+      console.log(posts);
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+
+    getCategories().then((data) => {
+      console.log(data);
+    });
+  }
+
   render() {
-    const { initial } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <h1>{initial}</h1>
+        <div className="post-list-container">
+          {posts.map(post => (
+            <article key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+              <span className="post-category">{post.category}</span>
+            </article>
+          ))}
+        </div>
       </div>
     );
   }
