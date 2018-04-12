162af5d81dac5523cbe0aaacde84a7d0e4741ebe
Fagner <fagnerschwalm@gmail.com>
2018-04-11 21:31:54 -0300

    chore: Setup jest and enzyme

--- src/components/App/__tests__/index.test.js
+++ src/components/App/__tests__/index.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../App';
+import * as ReadableAPI from '../../../api/ReadableAPI';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock('../../../api/ReadableAPI');
+
+describe('App component', () => {
+  beforeEach(() => {
+    mockHttpRequests();
+  });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
+
+function mockHttpRequests() {
+  ReadableAPI.getPosts = jest.fn(() => Promise.resolve([]));
+  ReadableAPI.getCategories = jest.fn(() => Promise.resolve([]));
+}
