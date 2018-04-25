9ab9b5401ca11d7745abe941a2965f4455b95cb5
Fagner <fagnerschwalm@gmail.com>
2018-04-25 00:34:47 -0300

    test: Wrap App with Provider - react-redux

--- src/components/App/__tests__/index.test.js
+++ src/components/App/__tests__/index.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './..';
 import * as ReadableAPI from '../../../api/ReadableAPI';
+import configureStore from '../../../configureStore';
 
 jest.mock('../../../api/ReadableAPI');
 
 describe('App component', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore();
+  });
+
   beforeEach(() => {
     mockHttpRequests();
   });
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
