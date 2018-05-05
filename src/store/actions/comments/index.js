5917f7ddac95b9df56e8d4c027005154addbf4c5
Fagner <fagnerschwalm@gmail.com>
2018-05-05 11:08:52 -0300

    feat: Create actions to fetch comments

---
+++ src/store/actions/comments/index.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actionTypes';
+import { getCommentsByPostId } from '../../../api/ReadableAPI';
+
+const fetchCommentsByPostId = id => async (dispatch) => {
+  dispatch(requestComments());
+  try {
+    const response = await getCommentsByPostId(id);
+    dispatch(receiveComments(response));
+  } catch (error) {
+    dispatch(errorOnFetchComments(error));
+  }
+};
+
+const requestComments = () => ({
+  type: actionTypes.FETCH_COMMENTS_BY_POST_ID,
+});
+
+const receiveComments = payload => ({
+  type: actionTypes.RECEIVE_COMMENTS_BY_POST_ID,
+  payload,
+});
+
+const errorOnFetchComments = payload => ({
+  type: actionTypes.ERROR_ON_FETCH_COMMENTS,
+  payload,
+});
+
+export { fetchCommentsByPostId };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,3 +3,5 @@ export { fetchCategories } from './categories';
 export { fetchPosts } from './posts';
 
 export { incrementVotePost, decrementVotePost, fetchPostById, deletePostById } from './post';
+
+export { fetchCommentsByPostId } from './comments';
