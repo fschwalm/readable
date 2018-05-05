2f1ab5c03acd9245d45c66059717ad0b964fd183
Fagner <fagnerschwalm@gmail.com>
2018-05-05 11:27:01 -0300

    feat: Fetch comments from PostPage component

--- src/components/PostPage/index.jsx
+++ src/components/PostPage/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CommentList from '../CommentsList';
 import Post from '../Post';
-import { fetchPostById } from '../../store/actions';
+import { fetchPostById, fetchCommentsByPostId } from '../../store/actions';
 
 const propTypes = {
   isFetchingPost: PropTypes.bool,
@@ -13,39 +13,17 @@ const defaultProps = {
   isFetchingPost: false,
 };
 
-const comments = [
-  {
-    id: '894tuq4ut84ut8v4t8wun89g',
-    parentId: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1468166872634,
-    body: 'Hi there! I am a COMMENT.',
-    author: 'thingtwo',
-    voteScore: 6,
-    deleted: false,
-    parentDeleted: false,
-  },
-  {
-    id: '8tu4bsun805n8un48ve89',
-    parentId: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1469479767190,
-    body: 'Comments. Are. Cool.',
-    author: 'thingone',
-    voteScore: -5,
-    deleted: false,
-    parentDeleted: false,
-  },
-];
-
 class PostPage extends React.Component {
   async componentDidMount() {
     this.props.onFetchPostById(this.props.match.params.id);
+    this.props.onfetchCommentsByPostId(this.props.match.params.id);
   }
   render() {
     return (
       <div className="post-list-container">
         {/* TODO: Try find a better solution to post[0]*/}
         {this.props.isFetchingPost === false && <Post post={this.props.post[0]} />}
-        <CommentList comments={comments} />
+        <CommentList comments={this.props.comments} />
       </div>
     );
   }
@@ -56,10 +34,15 @@ const mapStateToProps = state => ({
   isFetchingPost: state.postsReducer.isFetchingPost,
   hasErrorOnFetchPost: state.postsReducer.hasErrorOnFetchPost,
   fetchPostErrorMessage: state.postsReducer.fetchPostErrorMessage,
+  comments: state.commentsReducer.comments,
+  isFetchingComments: state.commentsReducer.isFetchingComments,
+  hasErrorOnFetchComments: state.commentsReducer.hasErrorOnFetchComments,
+  fetchCommentsErrorMessage: state.commentsReducer.fetchCommentsErrorMessage,
 });
 
 const mapDispatchToProps = dispatch => ({
   onFetchPostById: id => dispatch(fetchPostById(id)),
+  onfetchCommentsByPostId: id => dispatch(fetchCommentsByPostId(id)),
 });
 
 PostPage.propTypes = propTypes;
