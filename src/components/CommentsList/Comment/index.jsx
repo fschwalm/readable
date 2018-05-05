56eadc4b85fadc0a4dde67d7f73fa7ae2a24eaf0
Fagner <fagnerschwalm@gmail.com>
2018-05-05 12:11:21 -0300

    feat: Connect Comment component to store

--- src/components/CommentsList/Comment/index.jsx
+++ src/components/CommentsList/Comment/index.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Score from '../../Score';
 import './index.css';
+import {
+  incrementVoteComment,
+  decrementVoteComment,
+  deleteCommentById,
+} from '../../../store/actions';
 
 function Comment({
-  id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted,
+  id,
+  parentId,
+  timestamp,
+  body,
+  author,
+  voteScore,
+  deleted,
+  parentDeleted,
+  onIncrementVoteComment,
+  onDecrementVoteComment,
+  onDeleteComment,
 }) {
   return (
     <div className="comment">
+      <span className="delete-button">
+        <button onClick={() => onDeleteComment(id)}>X</button>
+      </span>
       <p>{body}</p>
       <p>
         <span>By </span>
         {author}
       </p>
-      <Score score={voteScore} />
+      <Score
+        score={voteScore}
+        onIncrementScore={() => onIncrementVoteComment(id)}
+        onDecrementScore={() => onDecrementVoteComment(id)}
+      />
     </div>
   );
 }
 
-export default Comment;
+const mapDispatchToProps = dispatch => ({
+  onIncrementVoteComment: id => dispatch(incrementVoteComment(id)),
+  onDecrementVoteComment: id => dispatch(decrementVoteComment(id)),
+  onDeleteComment: id => dispatch(deleteCommentById(id)),
+});
+
+export default connect(null, mapDispatchToProps)(Comment);
