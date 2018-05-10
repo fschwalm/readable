531029f8961270f240559db055c1bc526f4b87dc
Fagner <fagnerschwalm@gmail.com>
2018-05-09 22:11:54 -0300

    refactor: Move Router to App Component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import MenuBar from '../MenuBar';
+import PostsDisplayList from '../PostsDisplayList';
+import PostPage from '../PostPage';
+import NewPost from '../NewPost';
+import EditPost from '../EditPost';
+import NotFound from '../NotFound';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => (
+  <div className="App">
+    <Router>
+      <React.Fragment>
         <MenuBar />
-      </div>
-    );
-  }
-}
+        <Switch>
+          <Route path="/posts/new" component={NewPost} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/posts/edit/:id" component={EditPost} />
+          <Route path="/:category/:id" component={PostPage} />
+          <Route path="/:category?" component={PostsDisplayList} />
+        </Switch>
+      </React.Fragment>
+    </Router>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
-import PostsDisplayList from './components/PostsDisplayList';
-import PostPage from './components/PostPage';
-import NewPost from './components/NewPost';
-import EditPost from './components/EditPost';
-import NotFound from './components/NotFound';
+import './index.css';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <div>
-        <Route path="/" component={App} />
-        <Switch>
-          <Route path="/posts/new" component={NewPost} />
-          <Route path="/not-found" component={NotFound} />
-          <Route path="/posts/edit/:id" component={EditPost} />
-          <Route path="/:category/:id" component={PostPage} />
-          <Route path="/:category?" component={PostsDisplayList} />
-        </Switch>
-      </div>
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root'),
 );
+
 registerServiceWorker();
