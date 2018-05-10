fc4e7498ee094476e7353dfa02a7c9be32695fbd
Fagner <fagnerschwalm@gmail.com>
2018-05-10 19:06:57 -0300

    fix: Add missing atributes on Post

--- src/model/post.js
+++ src/model/post.js
@@ -2,7 +2,15 @@ import uuidv4 from 'uuid/v4';
 
 class Post {
   constructor({
-    id = uuidv4(), title = '', body = '', author = '', category = '', timestamp = Date.now(),
+    id = uuidv4(),
+    title = '',
+    body = '',
+    author = '',
+    category = '',
+    timestamp = Date.now(),
+    deleted = false,
+    voteScore = 1,
+    commentCount = 0,
   }) {
     this.id = id;
     this.title = title;
@@ -10,6 +18,9 @@ class Post {
     this.author = author;
     this.category = category;
     this.timestamp = timestamp;
+    this.deleted = deleted;
+    this.voteScore = voteScore;
+    this.commentCount = commentCount;
   }
 }
 
